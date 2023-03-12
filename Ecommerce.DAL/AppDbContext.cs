dbd27713cb63a9ecdf634c94e2553d7adab70d93
Denis <kuuushnirrr@gmail.com>
2023-03-12 12:04:06 +0200

    add app db context

---
+++ Ecommerce.DAL/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<CategoryAttribute> CategoryAttributes { get; set; }
+    public DbSet<CategoryAttributeName> CategoryAttributeNames { get; set; }
+    public DbSet<CategoryDiscount> Discounts { get; set; }
+    public DbSet<CategoryName> CategoryNames { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductAttribute> ProductAttributes { get; set; }
+    public DbSet<ProductAttributeName> ProductAttributeNames { get; set; }
+    public DbSet<ProductDescription> ProductDescriptions { get; set; }
+    public DbSet<ProductDiscount> ProductDiscounts { get; set; }
+    public DbSet<ProductImage> ProductImages { get; set; }
+    public DbSet<ProductTitle> ProductTitles { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    public DbSet<ShippingMethod> ShippingMethods { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
+
+    public AppDbContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("PGSQL_STRING") + "Database=ecommerce";
+        optionsBuilder.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+    }
+}
